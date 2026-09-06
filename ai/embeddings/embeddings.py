c197a04869f767d71b3bf592441c68888b1db0da
Thakuraditya7803 <adityathakur7803@gmail.com>
2026-09-06 15:30:38 +0530

    .

--- ai/embeddings/embeddings.py
+++ ai/embeddings/embeddings.py
@@ -1,16 +1,34 @@
-from sentence_transformers import SentenceTransformer
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+)
 
 
 class EmbeddingModel:
+
     def __init__(self):
-        print("Loading embedding model...")
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.model = "nvidia/nemotron-3-embed-1b:free"
 
     def generate_embedding(self, text: str):
-        return self.model.encode(text).tolist()
+
+        response = client.embeddings.create(
+            model=self.model,
+            input=text,
+            encoding_format="float",
+        )
+
+        return response.data[0].embedding
 
 
 if __name__ == "__main__":
+
     model = EmbeddingModel()
 
     text = "Students must maintain 75% attendance."
---
+++ api/main.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+response = requests.post(
+  url="https://openrouter.ai/api/v1/embeddings",
+  headers={
+    "Authorization": "Bearer <OPENROUTER_API_KEY>",
+    "Content-Type": "application/json",
+    "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+    "X-OpenRouter-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+  },
+  data=json.dumps({
+    "model": "nvidia/nemotron-3-embed-1b:free",
+    "input": "Your text string goes here",
+    # "input": ["text1", "text2", "text3"], # batch embeddings also supported!
+    "encoding_format": "float"
+  })
+)
